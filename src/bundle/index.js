726aa1cc77add6a826116de4163f7ac58383b29c
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-10-08 15:30:15 +0000

    It works... Sort of.

--- src/bundle/index.js
+++ src/bundle/index.js
@@ -3,6 +3,11 @@
 
 "use strict";
 
+if (document.body?.className.indexOf("mobile") > -1) {
+	// Reject on mobile
+	return;
+};
+
 // Global functions for quick element selection
 let qse = (selector, source = document) => {
 	return source.querySelector(selector);
@@ -14,15 +19,23 @@ let qsa = (selector, source = document) => {
 // Remove the scripting HTML widget as soon as execution starts
 qse(".widget.HTML").remove();
 
-let currentScheme = 0;
+// Try to find the action button
+let switchButton = qse("a[href='javascript:switchColourScheme()']");
 
-// If implemented, save colour scheme preferences here
+// Initialize the colour scheme
+let currentScheme = 0;
 
 // Helper for setting colour scheme
 let setScheme = (scheme = 0) => {
 	currentScheme = scheme % 3;
 	document.body.className = ["", "prefer-dark", "prefer-light"][scheme] || "";
+	switchButton.innerText = "ADL"[scheme] || "A";
 };
+
+// If implemented, load saved colour scheme preferences here
+
+// Apply loaded colour scheme
+setScheme(currentScheme);
 
 // Define a global colour switcher
 self.switchColourScheme = () => {
